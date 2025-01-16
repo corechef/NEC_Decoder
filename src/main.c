929067f63db94f2c88d0f830f5a786580447b7bc
corechef <corechef206@gmail.com>
2025-01-16 21:52:35 +0300

    add: state

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+#include <avr/io.h>
+
+enum nec_decoder_state_t
+{
+    NEC_DECODER_IDLE,
+    NEC_DECODER_HEADER,
+    NEC_DECODER_DECODING,
+    NEC_DECODER_STATE_MAX
+};
+
+enum nec_decoder_state_t state = 0;
+
 int main(void)
 {
     while (1) {}
